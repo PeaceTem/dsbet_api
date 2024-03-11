8d6f32734bafb3963737f7297862915d6eba9a79
PeaceTem <olumidejonathan10@gmail.com>
2024-03-11 07:06:04 +0100

    changed host name

--- config/config.exs
+++ config/config.exs
@@ -61,7 +61,7 @@ config :dsbet,
 
 # Configures the endpoint
 config :dsbet, DSBetWeb.Endpoint,
-  url: [host: "localhost"],
+  url: [host: System.get_env("PHX_HOST") <> ".gigalixirapp.com" || "localhost"],
   adapter: Phoenix.Endpoint.Cowboy2Adapter,
   render_errors: [
     formats: [html: DSBetWeb.ErrorHTML, json: DSBetWeb.ErrorJSON],
